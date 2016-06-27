cb64397337ef54aaab774e06d8cb3bcc08c0fee8
卞尔刚 <1179514628@qq.com>
2016-06-27 11:07:13 +0800

    fdasfdsaf

---
+++ src/main/java/com/bzt/spark/start/App.java
@@ -0,0 +1,15 @@
+package com.bzt.spark.start;
+import static spark.Spark.*;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main(String[] args) {
+        for(String arg : args){
+            System.out.println("arg -->" + arg);
+        }
+        get("/hello", (req, res) -> "いらっしゃいませxxxx");
+    }
+} 
